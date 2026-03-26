fe16c0f0487df66f99174c2e52626f7ad5d6cbfa
hoatrancntt-devops <hoatrancntt@gmail.com>
2026-03-26 22:47:24 +0700

    fix: locals project_slug + vm_template_id fallback

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,5 @@
 locals {
-  cfg = yamldecode(file("${path.module}/../project-config.yml"))
+  cfg            = yamldecode(file("${path.module}/../project-config.yml"))
+  project_slug   = replace(lower(local.cfg.project_name), "_", "-")
+  vm_template_id = try(local.cfg.vm_template_id, 9000)
 }
